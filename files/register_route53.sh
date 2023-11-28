9b16dd9085112cb05270c9f98c87274be26b83c9
kristian.tolev <kristian.tolev@ontotext.com>
2023-11-28 15:01:04 +0200

    Added variables in register route53

--- files/register_route53.sh
+++ files/register_route53.sh
@@ -7,6 +7,10 @@ until ping -c 1 google.com &> /dev/null; do
   sleep 5
 done
 
+imds_token=$( curl -Ss -H "X-aws-ec2-metadata-token-ttl-seconds: 300" -XPUT 169.254.169.254/latest/api/token )
+availability_zone=$( curl -Ss -H "X-aws-ec2-metadata-token: $imds_token" 169.254.169.254/latest/meta-data/placement/availability-zone )
+local_ipv4=$( curl -Ss -H "X-aws-ec2-metadata-token: $imds_token" 169.254.169.254/latest/meta-data/local-ipv4 )
+
 # Register the instance in Route 53, using the volume id for the sub-domain
   volume_id=$(
     aws --cli-connect-timeout 300 ec2 describe-volumes \
